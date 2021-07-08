39303d1df01450210831b1d2aa5cc902f124fe8f
JunHoBaek <bemodesty306@naver.com>
2021-07-08 23:21:19 +0900

    styles

--- js/background.js
+++ js/background.js
@@ -1,7 +1,4 @@
 const images = ["0.jpg", "1.jpg", "2.jpg"];
 const idx = Math.round(Math.random()*(images.length-1));
 
-const bgImage = document.createElement("img");
-bgImage.src = `img/${images[idx]}`;
-
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url('img/${images[idx]}')`;
